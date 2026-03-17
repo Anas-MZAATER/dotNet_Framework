ab5e384a1dbd9898f4f0ec9d9c494916fab569e9
Anas MZAATER <anasmzaater@gmail.com>
2026-03-17 22:14:49 +0000

    Ajoutez des fichiers projet.

---
+++ dotNet/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choisis le programme : 1 ou 2");
+            string choix = Console.ReadLine();
+
+            switch (choix)
+            {
+                case "1":
+                    Programme1.Lancer();
+                    break;
+                case "2":
+                    Programme2.Lancer();
+                    break;
+                default:
+                    Console.WriteLine("Choix invalide");
+                    break;
+            }
+        }
+    }
+    class Programme1
+    {
+        public static void Lancer()
+        {
+            Console.WriteLine("Programme 1 exécuté !");
+        }
+    }
+
+    class Programme2
+    {
+        public static void Lancer()
+        {
+            Console.WriteLine("Programme 2 exécuté !");
+        }
+    }
+}
