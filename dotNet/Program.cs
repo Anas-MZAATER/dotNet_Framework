5bde01c6e291b4e5179ccab3e94cb0817c01c870
Anas MZAATER <anasmzaater@gmail.com>
2026-03-17 22:23:03 +0000

    firstConsoleApp

--- dotNet/Program.cs
+++ dotNet/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace dotNet
 {
@@ -27,11 +24,100 @@ namespace dotNet
             }
         }
     }
+
     class Programme1
     {
         public static void Lancer()
         {
             Console.WriteLine("Programme 1 exécuté !");
+
+            Dictionary<string, Action> exercices = new Dictionary<string, Action>()
+            {
+                { "somme",       Somme       },
+                { "division",    Division    },
+                { "permutation", Permutation },
+                { "triangle",    Triangle    },
+                { "binaire",     Binaire     }
+            };
+
+            while (true)
+            {
+                Console.WriteLine("\nMéthodes disponibles :");
+                foreach (var key in exercices.Keys)
+                    Console.WriteLine("- " + key);
+
+                Console.Write("\nChoix : ");
+                string choix = Console.ReadLine().ToLower();
+
+                if (exercices.ContainsKey(choix))
+                    exercices[choix]();
+                else
+                    Console.WriteLine("Méthode non trouvée !");
+            }
+        }
+
+        static void Somme()
+        {
+            Console.Write("Entrez a : ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Entrez b : ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Somme = " + (a + b));
+        }
+
+        static void Division()
+        {
+            Console.Write("Entrez a : ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Entrez b : ");
+            double b = double.Parse(Console.ReadLine());
+
+            if (b != 0)
+                Console.WriteLine("Résultat = " + (a / b));
+            else
+                Console.WriteLine("Division par zéro !");
+        }
+
+        static void Permutation()
+        {
+            Console.Write("Entrez a : ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Entrez b : ");
+            int b = int.Parse(Console.ReadLine());
+
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"Après permutation : a={a}, b={b}");
+        }
+
+        static void Triangle()
+        {
+            Console.Write("Entrez le nombre : ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Entrez la largeur : ");
+            int largeur = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= largeur; i++)
+            {
+                for (int j = 0; j < i; j++)
+                    Console.Write(n);
+                Console.WriteLine();
+            }
+        }
+
+        static void Binaire()
+        {
+            Console.Write("Entrez un nombre : ");
+            int nombre = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Binaire = " + Convert.ToString(nombre, 2));
         }
     }
 
